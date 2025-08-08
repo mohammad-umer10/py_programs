efe1cdb1ea9e9a90354cb2ab90e8067e41e76267
mohammad-umer10 <mohammadumerabbasi10@gmail.com>
2025-08-08 06:47:43 -0700

    Day_02

---
+++ form_02.py
@@ -0,0 +1,13 @@
+greetings=("wellcome to google account")
+name=input("Enter your name: ")
+gmail=input("Please enter your gmail: ")
+password=input("Please enter your password: ")
+
+if gmail=="mohammadumerabbasi10@gmail.com" and password=='12345678':
+    print(greetings,name)
+
+else:
+    print("Gmail or password is incorrect")
+    
+  
+    
