3f41853343d6b9384ae2d770e1c1b0109a4383b4
mohammad-umer10 <mohammadumerabbasi10@gmail.com>
2025-08-09 11:51:22 -0700

    Day_03

---
+++ variable_types/Variables.py
@@ -0,0 +1,14 @@
+x=[1,2,3,4,5,6]
+y=(1,2,3,4,5,6)
+z={1,2,3,4,5,6}
+a={"name":"umer",  "class":10}
+
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
+print(a)
+print(type(a))
+print(a.values())
---
+++ variable_types/variable_types.py
@@ -0,0 +1,25 @@
+a = 5
+b = 'i am a student'
+c = [125 , 70 , 45]
+d = (56, 56, 79)
+e = {'ali' , 'asad', 'moiz'}
+f = {"name":"waleed", "class":10}
+g = 45*2j
+h = 'i','j'
+
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(h)
+print(type(h))
